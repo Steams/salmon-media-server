aac6e2e8ba5c91c8ed77a38c7bdd1dd8f8b205a8
steams <radclifferobs@gmail.com>
2019-11-22 17:41:00 -0500

    Add initial support for header based authentication hub

--- src/Salmon.hs
+++ src/Salmon.hs
@@ -3,38 +3,37 @@
 
 module Salmon(run) where
 
-import           Control.Concurrent        (forkIO, threadDelay)
+import           Control.Concurrent       (forkIO, threadDelay)
 import           Control.Concurrent.Async (mapConcurrently)
-import           Control.Monad             (forever)
-import qualified Data.ByteString.Lazy      as LB
-import           Data.Digest.Pure.MD5
+import           Control.Monad            (forever)
 import           Data.List
-import           Data.Set                  hiding (filter, isSuffixOf, map)
+import           Data.Set                 as Set hiding (filter, map)
 import           Network.HTTP.Simple
 import           Path
 import           Salmon.Data
 import           Salmon.FileSystem
 import           Salmon.Hls
-import           Salmon.Hub                as Hub
+import           Salmon.Hub               as Hub
 import           Salmon.Server
 import           System.FSNotify
 
-synch_with_hub :: IO ()
-synch_with_hub = do
+
+synch_with_hub :: Credentials -> IO ()
+synch_with_hub credentials = do
   local_files            <- get_files
   local_hashes           <- mapM to_hash local_files
-  remote_hashes_response <- Hub.get_hashes
-  let remote_hashes      =  getResponseBody remote_hashes_response
-  let new_hashes         =  difference ( fromList local_hashes  ) ( fromList remote_hashes )
-  let deleted_hashes     =  difference ( fromList remote_hashes ) ( fromList local_hashes  )
-  let new_files          =  filter (\(file,hash) -> elem hash new_hashes) (zip local_files local_hashes)
+  remote_hashes_response <- Hub.get_hashes credentials
+  let remote_hashes       = getResponseBody remote_hashes_response
+  let new_hashes          = difference ( Set.fromList local_hashes  ) ( Set.fromList remote_hashes )
+  let deleted_hashes      = difference ( Set.fromList remote_hashes ) ( Set.fromList local_hashes  )
+  let new_files           = filter (\(file,hash) -> elem hash new_hashes) (zip local_files local_hashes)
   new_tracks             <- mapConcurrently parse_track (map fst new_files)
   mapM_ print new_tracks
   mapM_ print deleted_hashes
-  let new_media          =  zipWith track_to_media new_files new_tracks
-  response               <- post_songs new_media
+  let new_media           = zipWith track_to_media new_files new_tracks
+  response               <- Hub.post_songs credentials new_media
   print . getResponseStatus $ response
-  response               <- delete_songs (toList deleted_hashes)
+  response               <- Hub.delete_songs credentials (toList deleted_hashes)
   print . getResponseStatus $ response
 
 
@@ -45,52 +44,60 @@ initialize_playlists = do
   mapM_ (forkIO . generate_playlist) files
 
 
+watch_predicate :: Event -> Bool
 watch_predicate =
   \case
-    Added path time _ ->
+    Added path _ _ ->
       if isSuffixOf ".mp3" path
         then True
         else False
-
-    Removed path time _ ->
+    Removed path _ _ ->
       if isSuffixOf ".mp3" path
         then True
         else False
-    _ -> False
+    Modified _ _ _ -> False
+    Unknown _ _ _ -> False
 
 
-process_update  = \case
-    Added path time _ -> do
+process_update :: Credentials -> Event -> IO ()
+process_update credentials =
+  \case
+    Added path _ _ -> do
       file_path    <- parseAbsFile path
       threadDelay 1000000
       hash         <- to_hash file_path
       track        <- parse_track file_path
       putStr "Printing prased track :: "
       print track
-      let media = track_to_media (file_path,hash) track
+      let media     = track_to_media (file_path, hash) track
       generate_playlist file_path
-      response     <- post_songs [media]
+      response     <- Hub.post_songs credentials [media]
       print . getResponseStatus $ response
 
-    Removed path time _ -> do
+    Removed _ _ _ -> do
       local_files  <- get_files
       local_hashes <- mapM to_hash local_files
-      res          <- Hub.get_hashes
-      let remote_hashes   = getResponseBody res
-      let deleted_hashes  =  difference ( fromList remote_hashes ) ( fromList local_hashes  )
+      res          <- Hub.get_hashes credentials
+      let remote_hashes  = getResponseBody res
+      let deleted_hashes = difference (Set.fromList remote_hashes) (Set.fromList local_hashes)
       mapM_ print deleted_hashes
-      response     <- delete_songs (toList deleted_hashes)
+      response     <- Hub.delete_songs credentials (toList deleted_hashes)
       print . getResponseStatus $ response
 
-watch_fs =
+    Modified _ _ _ -> return ()
+    Unknown _ _ _ -> return ()
+
+watch_fs :: Credentials -> IO a
+watch_fs credentials =
   withManager $ \mgr -> do
-    watchDir mgr library_path watch_predicate process_update
+    _ <- watchDir mgr library_path watch_predicate (process_update credentials)
     forever $ threadDelay 1000000
 
 run :: IO ()
 run = do
   initialize_playlists
-  synch_with_hub
+  synch_with_hub (Credentials "1")
   putStrLn "settup complete"
-  watch_fs
-  -- run_server
+  _ <- forkIO $ watch_fs (Credentials "1")
+  _ <- forkIO run_server
+  forever $ threadDelay 1000000
--- src/Salmon/Hub.hs
+++ src/Salmon/Hub.hs
@@ -1,17 +1,31 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Salmon.Hub(post_songs, get_hashes, delete_songs) where
+module Salmon.Hub(post_songs, get_hashes, delete_songs, Credentials(..)) where
 
+import           Data.ByteString.Char8
 import           Network.HTTP.Simple
+import           Network.HTTP.Types.Header
 import           Salmon.Data
 
-post_songs :: [Media] -> IO (Response (Either JSONException ()))
-post_songs songs =
-  httpJSONEither $ setRequestBodyJSON songs "POST http://127.0.0.1:8080/media"
 
-get_hashes :: IO (Response [String])
-get_hashes = httpJSON "http://127.0.0.1:8080/synch"
 
-delete_songs :: [String] -> IO (Response (Either JSONException ()))
-delete_songs hashes =
-  httpJSONEither $ setRequestBodyJSON hashes "POST http://127.0.0.1:8080/synch"
+
+newtype Credentials = Credentials String
+
+post_songs :: Credentials -> [Media] -> IO (Response (Either JSONException ()))
+post_songs (Credentials user_id) songs =
+  httpJSONEither $
+  addRequestHeader hAuthorization (pack user_id) $
+  setRequestBodyJSON songs "POST http://127.0.0.1:8080/media"
+
+get_hashes :: Credentials -> IO (Response [String])
+get_hashes (Credentials user_id) =
+  httpJSON $
+  addRequestHeader hAuthorization (pack user_id) $
+  "http://127.0.0.1:8080/synch"
+
+delete_songs :: Credentials -> [String] -> IO (Response (Either JSONException ()))
+delete_songs (Credentials user_id) hashes =
+  httpJSONEither $
+  addRequestHeader hAuthorization (pack user_id) $
+  setRequestBodyJSON hashes "POST http://127.0.0.1:8080/synch"
