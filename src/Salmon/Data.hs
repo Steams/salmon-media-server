52bb8bed7e820195b06e2efa1bea4f992da9edcc
steams <radclifferobs@gmail.com>
2019-11-24 13:51:52 -0500

    Extract additional meta data from audio files

--- src/Salmon/Data.hs
+++ src/Salmon/Data.hs
@@ -9,27 +9,38 @@ import           Salmon.FileSystem
 import           Salmon.Server        (url_path)
 import           Sound.HTagLib
 
-data Media = Media { title :: String , duration :: Int, path :: String, hash :: String} deriving (Show)
+data Media = Media { title :: String , artist :: String, album :: String, duration :: Int, path :: String, hash :: String} deriving (Show)
 
 instance ToJSON Media where
-  toJSON (Media title duration playlist hash) =
-    object ["title" .= title, "duration" .= duration, "playlist" .= playlist, "hash" .= hash]
+  toJSON (Media title artist album duration playlist hash) =
+    object
+      [ "title" .= title
+      , "artist" .= artist
+      , "album" .= album
+      , "duration" .= duration
+      , "playlist" .= playlist
+      , "hash" .= hash
+      ]
 
 data AudioTrack =
   AudioTrack
     { atTitle    :: Title
     , atDuration :: Duration
+    , atArtist   :: Artist
+    , atAlbum    :: Album
     }
   deriving (Show)
 
 audio_track_getter :: TagGetter AudioTrack
-audio_track_getter = AudioTrack <$> titleGetter <*> durationGetter
+audio_track_getter = AudioTrack <$> titleGetter <*> durationGetter <*> artistGetter <*> albumGetter
 
                      -- TODO USE NEW TYPES TO PREVENT SENDING NAME AND HASH IN WRONG ORDER
 track_to_media :: (Path Abs File,String) -> AudioTrack -> Media
-track_to_media (file, file_hash) (AudioTrack title duration) =
+track_to_media (file, file_hash) (AudioTrack title duration artist album) =
   Media
     (drop 6 . show $ title) -- Drop 6 removes the "Title " label that show places at the front of the string
+    (drop 6 . show $ artist)
+    (drop 6 . show $ album)
     (unDuration duration)
     (url_path $ get_file_name file)
     (file_hash)
