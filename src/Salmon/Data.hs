2f3c5044ea12b85b054d34926a784b5bec4db61e
steams <radclifferobs@gmail.com>
2019-12-15 21:41:39 -0500

    Extract and send track number in media meta data

--- src/Salmon/Data.hs
+++ src/Salmon/Data.hs
@@ -9,15 +9,27 @@ import           Salmon.FileSystem
 import           Salmon.Server        (url_path,art_url)
 import           Sound.HTagLib
 
-data Media = Media { title :: String , artist :: String, album :: String, duration :: Int, path :: String, art :: String, hash :: String} deriving (Show)
+data Media =
+  Media
+    { title :: String
+    , artist :: String
+    , album :: String
+    , duration :: Int
+    , number :: Maybe Int
+    , path :: String
+    , art :: String
+    , hash :: String
+    }
+  deriving (Show)
 
 instance ToJSON Media where
-  toJSON (Media title artist album duration playlist art hash) =
+  toJSON (Media title artist album duration number playlist art hash) =
     object
       [ "title" .= title
       , "artist" .= artist
       , "album" .= album
       , "duration" .= duration
+      , "number" .= number
       , "playlist" .= playlist
       , "art" .= art
       , "hash" .= hash
@@ -29,20 +41,22 @@ data AudioTrack =
     , atDuration :: Duration
     , atArtist   :: Artist
     , atAlbum    :: Album
+    , atTrack    :: Maybe TrackNumber
     }
   deriving (Show)
 
 audio_track_getter :: TagGetter AudioTrack
-audio_track_getter = AudioTrack <$> titleGetter <*> durationGetter <*> artistGetter <*> albumGetter
+audio_track_getter = AudioTrack <$> titleGetter <*> durationGetter <*> artistGetter <*> albumGetter <*> trackNumberGetter
 
-                     -- TODO USE NEW TYPES TO PREVENT SENDING NAME AND HASH IN WRONG ORDER
+-- TODO USE NEW TYPES TO PREVENT SENDING NAME AND HASH IN WRONG ORDER
 track_to_media :: (Path Abs File,String) -> AudioTrack -> Media
-track_to_media (file, file_hash) (AudioTrack title duration artist album) =
+track_to_media (file, file_hash) (AudioTrack title duration artist album track_number) =
   Media
     (read . drop 6 . show $ title) -- Drop 6 removes the "Title " label that show places at the front of the string
     (read . drop 6 . show $ artist)
     (read . drop 6 . show $ album)
     (unDuration duration)
+    (unTrackNumber <$> track_number)
     -- TODO path should be based on hash not name incase of name clash between albums
     (url_path file_hash)
     -- TODO This is silly fix later
